6e6f23d54a0f26797a07cfed1fdda89d9b1d8327
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-28 12:58:06 +0530

    m

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-# module "docdb"{
-# source           =    "./vendor/modules/vpc"
-# ENV              =    var.ENV
+module "docdb"{
+source           =    "./vendor/modules/vpc"
+ENV              =    var.ENV
 
-# }
+}
 
-# module "elasticdb"{
-# source           =    "./vendor/modules/elastic"
-# ENV              =    var.ENV
+module "elasticdb"{
+source           =    "./vendor/modules/elastic"
+ENV              =    var.ENV
 
-# }
+}
 
 module "mysqldb"{
 source           =    "./vendor/modules/mysqldb"
